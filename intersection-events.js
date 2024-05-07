f41a68f3172a83a27a9e79429503bf7821c0dfc7
Maurice van Creij <maurice.vancreij@webqem.com>
2024-05-07 13:50:10 +1000

    Add comments

--- intersection-events.js
+++ intersection-events.js
@@ -1,20 +1,31 @@
 /*! Intersection Events v0.0.1 - (c) 2024 Maurice van Creij - MIT License - https://github.com/WoollyMittens */
 
+// public handler class
 export class IntersectionEvents {
 	constructor(options) {
+		// define the default options
 		const defaults = {
 			root: null,
 			rootMargin: "0px 0px 0px 0px",
-			threshold: [0]
+			threshold: [0],
 		};
-		this.options = {...defaults, ...options};
+		// override the default options
+		this.options = { ...defaults, ...options };
+		// start the intersection observer
 		this.observer = new IntersectionObserver(this.handle.bind(this), this.options);
 	}
+
+	// adds an element to the observed list
 	watch(elem) {
+		// observe the given element
 		this.observer.observe(elem);
 	}
+
+	// triggers an event for every intersection
 	handle(intersections, observer) {
+		// for every intersection change
 		for (let intersection of intersections) {
+			// construct a custom event
 			let evt = new CustomEvent("visible", {
 				bubbles: true,
 				cancelable: false,
@@ -23,9 +34,11 @@ export class IntersectionEvents {
 					observer: observer,
 				},
 			});
+			// and dispatch it
 			intersection.target.dispatchEvent(evt);
 		}
 	}
 }
 
+// expose the main class (disable if importing)
 window.IntersectionEvents = IntersectionEvents;
